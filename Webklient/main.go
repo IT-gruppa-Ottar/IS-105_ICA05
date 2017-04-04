985ae300896711df7fa83b113230b6a701002a72
Christoffer Lislevand <chrirl16@student.uia.no>
2017-04-04 14:33:25 +0200

    Klar for førsteutkast  + Små justeringer og lagt inn kommentarer

--- Webklient/main.go
+++ Webklient/main.go
@@ -10,7 +10,10 @@ import (
 	"io/ioutil"
 )
 
-type currencyRatings struct {
+/**
+Struckt 1 - Viser konversjosnraten fra norsk kr til Britisk pund, US dollar og euro
+ */
+type CurrencyRatings struct {
 	Base string `json:"base"`
 	Date string `json:"date"`
 	Rates struct {
@@ -20,6 +23,9 @@ type currencyRatings struct {
 	} `json:"rates"`
 }
 
+/**
+Struckt 2 - Viser antall påloggede Steam-spillere
+ */
 type Steam struct {
 	Response struct {
 		PlayerCount int `json:"player_count"`
@@ -27,6 +33,10 @@ type Steam struct {
 	} `json:"response"`
 }
 
+/**
+Struckt 3 - Viser antall påloggede spiller på Battlefield
+og det største antallet som har vært pålogget de siste 24t.
+ */
 type Battlefield struct {
 	Response struct {
 		PC int `json:"count"`
@@ -34,6 +44,9 @@ type Battlefield struct {
 	} `json:"pc"`
 }
 
+/**
+Struckt 4 - Viser hvor mange mennesker som befinner seg i verdensrommet
+ */
 type SpaceType struct {
 	People []struct {
 		Craft string `json:"craft"`
@@ -43,6 +56,9 @@ type SpaceType struct {
 	Number int `json:"number"`
 }
 
+/**
+Struckt 5 - Viser informasjon om Batman filmen "The Dark Knight"
+ */
 type DarkKnight struct {
 	Title    string `json:"Title"`
 	Year     string `json:"Year"`
@@ -52,20 +68,39 @@ type DarkKnight struct {
 }
 
 
+
+/**
+Main - Laster inn dataene igjennom "loadUrl" og lagrer de i variabler som brukes for å kjøre printefunksjonene
+ */
 func main(){
 	api1 := loadUrl("http://api.fixer.io/latest?base=NOK")
-	currency(api1)
+	Currency(api1)
+	fmt.Println("---------------------------------------------------------------")
+
 	api2 := loadUrl("https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?format=json&appid=0")
 	SteamPlayers(api2)
-	api3 := loadUrl("http://api.open-notify.org/astros.json")
-	Space(api3)
-	api4 := loadUrl("http://api.bf4stats.com/api/onlinePlayers?output=json")
-	BattlefieldPlayers(api4)
+	fmt.Println("---------------------------------------------------------------")
+
+	api3 := loadUrl("http://api.bf4stats.com/api/onlinePlayers?output=json")
+	BattlefieldPlayers(api3)
+	fmt.Println("---------------------------------------------------------------")
+
+	api4 := loadUrl("http://api.open-notify.org/astros.json")
+	Space(api4)
+	fmt.Println("---------------------------------------------------------------")
+
 	api5 := loadUrl("http://www.omdbapi.com/?t=the+dark+knight&y=&plot=short&r=json")
 	TheDarkKnight(api5)
+	fmt.Println("---------------------------------------------------------------")
 }
 
+/**
+loadUrl
+@param String - En url som inneholder json
+@return String - Returner innholdet i url-en i en string som kan brukes til å printe data med
+ */
 func loadUrl(url string) string {
+	//Printer ut url-en som er brukt
 	fmt.Printf("Data from %s\n\n", url)
 	resp, err := http.Get(url)
 	// handle the error if there is one
@@ -85,6 +120,26 @@ func loadUrl(url string) string {
 	return content
 }
 
+/**
+Funksjonene under bruker samme "mal", men har sine egne tilpasninger for å printe ut "relevant" infromajson istedenfor json.
+Funksjonene henter informasjonen via struckt-ene.
+@param String - Stringen som returneres fra "loadUrl"
+ */
+func Currency(input string){
+	dec := json.NewDecoder(strings.NewReader(input))
+	for {
+		var c CurrencyRatings
+
+		if err := dec.Decode(&c); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Currency rates as of %q from %q\n", c.Date, c.Base)
+		fmt.Printf("USD: %.2f\nEUR: %.2f\nGBP: %.2f\n", c.Rates.USD, c.Rates.EUR, c.Rates.GBP)
+	}
+}
+
 func SteamPlayers(input string){
 	dec := json.NewDecoder(strings.NewReader(input))
 	for {
@@ -96,12 +151,12 @@ func SteamPlayers(input string){
 			log.Fatal(err)
 		}
 
-		fmt.Println("Steam Players")
-		fmt.Println( c.Response.PlayerCount)
+		fmt.Print("Steam Players: ")
+		fmt.Println(c.Response.PlayerCount)
 	}
 }
 
-func BattlefieldPlayers (input string){
+func BattlefieldPlayers(input string){
 	dec := json.NewDecoder(strings.NewReader(input))
 	for {
 		var c Battlefield
@@ -111,43 +166,10 @@ func BattlefieldPlayers (input string){
 		} else if err != nil {
 			log.Fatal(err)
 		}
-
-		fmt.Println("Battlefield Players")
+		fmt.Print("Battlefield Players: ")
 		fmt.Println( c.Response.PC)
-		fmt.Println("Battlefield peak last 24h")
+		fmt.Print("Battlefield peak last 24h: ")
 		fmt.Println( c.Response.Peak24)
-	}
-}
-
-func Space(input string){
-	dec := json.NewDecoder(strings.NewReader(input))
-	for {
-		var c SpaceType
-
-		if err := dec.Decode(&c); err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("People in space")
-		fmt.Println( c.Number)
-	}
-}
-
-//Code for currency
-func currency(input string){
-	dec := json.NewDecoder(strings.NewReader(input))
-	for {
-		var c currencyRatings
-
-		if err := dec.Decode(&c); err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Currency rates as of %q from %q\n", c.Date, c.Base)
-		fmt.Printf("USD: %.2f\nEUR: %.2f\nGBP: %.2f\n", c.Rates.USD, c.Rates.EUR, c.Rates.GBP)
 	}
 }
 
@@ -161,12 +183,25 @@ func TheDarkKnight(input string){
 		} else if err != nil {
 			log.Fatal(err)
 		}
-
 		fmt.Println("Movie Information")
-		fmt.Println("Movie Title:", c.Title)
-		fmt.Println("Rated:", c.Rated)
-		fmt.Println("Release Date", c.Released)
-		fmt.Println("Release Year", c.Year)
+		fmt.Println("Movie Title: ", c.Title)
+		fmt.Println("Rated: ", c.Rated)
+		fmt.Println("Release Date: ", c.Released)
+		fmt.Println("Release Year: ", c.Year)
+	}
+}
 
+func Space(input string){
+	dec := json.NewDecoder(strings.NewReader(input))
+	for {
+		var c SpaceType
+
+		if err := dec.Decode(&c); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print("People in space: ")
+		fmt.Println( c.Number)
 	}
 }
